a091401a15fe718b0597cdbdb281426ffac991b9
Cesar Cachay <cesarcachay1996@gmail.com>
2019-06-12 16:47:35 -0500

    Add validation for submit a budget & show error

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Question() {
-  // handleSubmit(event) {
-  //   console.log(event)
-  // }
+  const [quantity, setQuantity] = useState(0);
+  const [error, setError] = useState(false);
+
+  const setBudget = e => {
+    e.preventDefault();
+
+    console.log(typeof quantity);
+    // validation
+    if (quantity < 1 || isNaN(quantity)) {
+      setError(true);
+      return;
+    }
+    //
+  };
 
   return (
     <>
       <h2>Define your Budget</h2>
-      <form>
+
+      {error ? (
+        <p className="alert alert-danger error ">
+          The budget is not defined, please enter a budget
+        </p>
+      ) : null}
+
+      <form onSubmit={setBudget}>
         <input
           type="number"
           className="u-full-width"
           placeholder="Add your budget"
-          //  onChange={handleSubmit}
+          onChange={e => setQuantity(parseInt(e.target.value, 10))} //The 10 is because we define base 10 integers
         />
         <input
           type="submit"
